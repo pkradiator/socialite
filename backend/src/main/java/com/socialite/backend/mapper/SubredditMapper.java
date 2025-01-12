a256be875d9eed43ff69246b9553f528328b9bdc
pkradiator <kalalpriyanshu7@gmail.com>
2025-01-12 09:51:20 +0100

    Added Mapper

---
+++ backend/src/main/java/com/socialite/backend/mapper/SubredditMapper.java
@@ -0,0 +1,25 @@
+package com.socialite.backend.mapper;
+
+import com.socialite.backend.dto.SubredditDto;
+import com.socialite.backend.model.Post;
+import com.socialite.backend.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
